9c04e5fc343000c448d22e49e35c713730df6891
Joshua Hecker <jhecker2001@gmail.com>
2023-11-02 10:56:16 -0500

    Added function to set up the screen for the game.

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas
+from turtle import Screen
+
+
+def setup_screen():
+    screen = Screen()
+    screen.title("U.S. States Game")
+    return screen
+
+
+def main():
+    screen = setup_screen()
+
+
+main()
