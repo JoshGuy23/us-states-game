1a2ed62560080ace8671f4354b156da6075ca603
Joshua Hecker <jhecker2001@gmail.com>
2023-11-02 12:12:57 -0500

    Added functionality to stop guessing and add states not guessed to a csv file for the user to review later.

--- main.py
+++ main.py
@@ -25,12 +25,15 @@ def game(screen):
 
     states_guessed = 0
     guess_list = []
-    while len(guess_list) < 50 or answer_state != "quit":
+    while len(guess_list) < 50:
         answer_state = screen.textinput(title=f"{len(guess_list)}/50 States Correct",
                                         prompt="What's another state's name?")
 
         if answer_state is not None:
             answer_state = answer_state.title()
+
+        if answer_state is None or answer_state == "Quit":
+            break
 
         if answer_state in state_list and answer_state not in guess_list:
             data_index = data[data.state == answer_state].x.index
@@ -39,6 +42,18 @@ def game(screen):
             writer.goto(x=x_pos, y=y_pos)
             writer.write(arg=answer_state)
             guess_list.append(answer_state)
+    file_name = "states_to_learn.csv"
+    miss_list = []
+    if len(guess_list) < 50:
+        for state in state_list:
+            if state not in guess_list:
+                miss_list.append(state)
+    states_missed = {
+        "States Missed: ": miss_list
+    }
+    output = pandas.DataFrame(states_missed)
+    output.to_csv(file_name)
+
 
 
 def main():
